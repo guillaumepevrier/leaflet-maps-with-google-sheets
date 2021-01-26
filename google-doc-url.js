7f39767efaa22e6f7de8933465124851e48086ae
guillaumepevrier <77462991+guillaumepevrier@users.noreply.github.com>
2021-01-26 21:08:04 +0100

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -2,4 +2,4 @@
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1RS3y9k69q7ml4BpuWtGLMyh3kEzt6t4YsWNP5JVlfaA/edit#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = 'AIzaSyAaLzQd_qAa_nNcJlUjXVyzondD1j1AWdY';
